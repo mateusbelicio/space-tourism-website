28cd2686d4072fb497e597fd4ab8ff59e45230b0
Mateus Belicio <mateusbelicio@hotmail.com>
2023-01-06 15:46:38 -0300

    Fix button menu

--- src/javascript/controller.js
+++ src/javascript/controller.js
@@ -4,6 +4,8 @@ import { crewView } from './view/crew.js';
 import { technologyView } from './view/technology.js';
 
 const { crew, destinations, technology } = model.getData();
+const buttonMenu = document.querySelector('.button--menu');
+const navigation = document.querySelector('.navigation--primary');
 
 const controlDestination = function (tab) {
   destinationView.updateContent(destinations[tab], tab);
@@ -17,10 +19,39 @@ const controlTechnology = function (tab) {
   technologyView.updateContent(technology[tab], tab);
 };
 
+const resetButtonState = function () {
+  if (window.screen.width >= 640) {
+    buttonMenu.dataset.active = 'false';
+    navigation.setAttribute('aria-expanded', buttonMenu.dataset.active);
+    window.removeEventListener('resize', resetButtonState);
+  }
+};
+
+const closeOnPressEscape = function (keyPressed) {
+  if (keyPressed.key === 'Escape') {
+    buttonMenu.dataset.active = 'false';
+    navigation.setAttribute('aria-expanded', buttonMenu.dataset.active);
+    window.removeEventListener('keydown', closeOnPressEscape);
+  }
+};
+
+const addHandlerMenu = function () {
+  buttonMenu.addEventListener('click', () => {
+    buttonMenu.dataset.active = buttonMenu.dataset.active === 'true' ? 'false' : 'true';
+    navigation.setAttribute('aria-expanded', buttonMenu.dataset.active);
+
+    if (buttonMenu.dataset.active === 'true') {
+      window.addEventListener('resize', resetButtonState);
+      window.addEventListener('keydown', closeOnPressEscape);
+    }
+  });
+};
+
 const init = function () {
   destinationView.addHandleController(controlDestination);
   crewView.addHandleController(controlCrew);
   technologyView.addHandleController(controlTechnology);
+  addHandlerMenu();
 };
 
 init();
